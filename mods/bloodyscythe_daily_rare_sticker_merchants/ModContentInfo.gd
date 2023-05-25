a9dbbd3374aa9811e09b4805587813d7ad8c4015
Dave <16521341+davidmfritz@users.noreply.github.com>
2023-05-25 13:50:19 +0200

    Added enable toggle and ratio slider to settings

--- mods/bloodyscythe_daily_rare_sticker_merchants/ModContentInfo.gd
+++ mods/bloodyscythe_daily_rare_sticker_merchants/ModContentInfo.gd
@@ -1,10 +1,35 @@
 extends ContentInfo
 
+var setting_toggle_enabled: bool = true
+export var setting_slider_extra_sticker_upgrade_ratio: float = 0.5
+
+const MODUTILS: Dictionary = {
+	"settings": [
+		{
+			"property": "setting_toggle_enabled",
+			"type": "toggle",
+			"label": "Enabled",
+		},
+		{
+			"property": "setting_slider_extra_sticker_upgrade_ratio",
+			"type": "slider",
+			"label": "Extra Sticker Upgrade Ratio",
+			"min_value": 0.5,
+			"max_value": 1.0,
+			"step": 0.1,
+		},
+	],
+}
+
 func init_content() -> void:
 	assert(DLC.has_mod("cat_modutils", 0))
 	var modutils: ContentInfo = DLC.mods_by_id["cat_modutils"]
 	modutils.callbacks.connect_scene_ready("res://world/maps/interiors/TownHall.tscn", self, "_on_TownHall_ready")
 
 func _on_TownHall_ready(scene: Spatial) -> void:
+	if !setting_toggle_enabled:
+		return
+	
 	var mod_scene: Spatial = preload("CustomTownHallSpawn.tscn").instance()
+	mod_scene.set("setting_slider_extra_sticker_upgrade_ratio", setting_slider_extra_sticker_upgrade_ratio)
 	scene.add_child(mod_scene)
--- mods/bloodyscythe_daily_rare_sticker_merchants/world/behaviors/generic_npc/CustomMerchantNPCBehavior.gd
+++ mods/bloodyscythe_daily_rare_sticker_merchants/world/behaviors/generic_npc/CustomMerchantNPCBehavior.gd
@@ -1,13 +1,13 @@
 extends "res://world/behaviors/generic_npc/MerchantNPCBehavior.gd"
 
-export (float) var additional_stock_stat_multiplier:float = 1
-
 func _daily_slice(rand:Random, arr:Array, num:int)->Array:
 	if num <= 0:
 		return arr
 	
+	var modutils: ContentInfo = DLC.mods_by_id["bloodyscythe_daily_rare_sticker_merchants"]
+	var ratio = modutils.setting_slider_extra_sticker_upgrade_ratio
 	if additional_stock_stat_key != "":
-		num += floor(SaveState.stats.get_stat("exchange_purchased").get_count(additional_stock_stat_key) * additional_stock_stat_multiplier)
+		num += floor(SaveState.stats.get_stat("exchange_purchased").get_count(additional_stock_stat_key) * ratio)
 	
 	rand.shuffle(arr)
 	return arr.slice(0, num - 1)
